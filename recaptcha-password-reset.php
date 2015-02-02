a588e4f4ef92df815005e97e0b0f515f33eca3df
Ivan Kruchkoff <ivank@10up.com>
2015-02-02 21:57:55 +0700

    Added ReCAPTCHA to forgot password form

---
+++ recaptcha-password-reset.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+Plugin Name: WP Reset Password Form with reCAPTCHA
+Description: Add reCAPTCHA to WordPress Lost password form
+Version: 1.0
+Author: Ivan Kruchkoff
+Author URI: http://10up.com
+License: GPL2
+*/
+
+
+class ReCAPTCHA_Password_Reset_Form {
+
+	private $site_key;
+	private $secret;
+
+	public function __construct() {
+		$this->site_key  = 'REPLACEME';
+		$this->secret = 'REPLACEME';
+
+		// adds the captcha to the password reset form
+		add_action( 'lostpassword_form', array( $this, 'action_lostpassword_form' ) );
+		add_action( 'login_head', array( $this, 'action_login_head' ) );
+
+		// authenticate the captcha answer
+		add_action( 'lostpassword_post', array( $this, 'action_lostpassword_post' ) );
+	}
+
+
+	public function action_login_head() {
+		wp_enqueue_script( 'recaptcha', '//www.google.com/recaptcha/api.js' );
+
+	}
+
+	public function action_lostpassword_form() {
+		echo "<div class='g-recaptcha' data-sitekey='{$this->site_key}'></div>";
+	}
+
+
+
+	/**
+	 * Verify the captcha answer
+	 *
+	 * @return WP_Error
+	 */
+	public function action_lostpassword_post() {
+
+		if ( ! isset( $_POST['g-recaptcha-response'] ) || empty( $_POST['g-recaptcha-response'] ) ) {
+			return new WP_Error( 'empty_captcha', 'CAPTCHA should not be empty');
+		}
+
+		if( isset( $_POST['g-recaptcha-response'] ) && $this->recaptcha_response() == 'false' ) {
+			return new WP_Error( 'invalid_captcha', 'CAPTCHA response was incorrect');
+		}
+
+	}
+
+
+	/**
+	 * Get the reCAPTCHA API response.
+	 *
+	 * @return bool
+	 */
+	public function recaptcha_response() {
+
+		$response  = isset( $_POST['g-recaptcha-response'] ) ? esc_attr( $_POST['g-recaptcha-response'] ) : '';
+
+		$remote_ip = $_SERVER["REMOTE_ADDR"];
+
+		if ( strlen( $remote_ip ) && strlen( $response ) ) {
+			$response = wp_remote_get( "https://www.google.com/recaptcha/api/siteverify?secret={$this->secret}&response={$response}&remoteip={$remote_ip}" );
+			if ( $response['response']['code'] !== 200 ) {
+				return false;
+			}
+			$body = json_decode( $response['body'], true );
+
+			return $body[ 'success' ] == true;
+		}
+
+		return false;
+
+	}
+}
+
+new ReCAPTCHA_Password_Reset_Form();
